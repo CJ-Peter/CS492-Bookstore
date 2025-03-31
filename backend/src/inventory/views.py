8ff4cee6d0d9ae34ff0d2261e170180e2aa87766
CJ-Peter <143010297+CJ-Peter@users.noreply.github.com>
2025-03-30 18:58:08 -0600

    Revert "Backendsetup"

--- backend/src/inventory/views.py
+++ backend/src/inventory/views.py
@@ -1,10 +1,3 @@
-from rest_framework import viewsets
-from .models import Book
-from .serializers import BookSerializer
+from django.shortcuts import render
 
-class BookViewSet(viewsets.ModelViewSet):
-    """
-    A viewset for viewing and editing book instances.
-    """
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+# Create your views here.
